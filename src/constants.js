721061f1bbf328a310e7830cf0ca8833193b5f08
Abhay-1718 <abhaysingh80766@gmail.com>
2025-02-17 22:22:56 +0530

    change in constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,1 @@
-export const VITE_BACKEND_URL = 
-  location.hostname === "localhost" ? "http://localhost:3000" : "http://15.206.179.46:3000";
+export const VITE_BACKEND_URL = 'http://15.206.179.46:3000';
