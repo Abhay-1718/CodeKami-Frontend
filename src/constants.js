a8e2ed8239ed277512735ee9a997b45c3f0b0a93
Abhay singh <154751247+Abhay-1718@users.noreply.github.com>
2025-02-19 12:15:14 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@
 // export const VITE_BACKEND_URL = 'https://www.codekami.online/:5000';
 export const VITE_BACKEND_URL = location.hostname === "localhost" 
   ? "http://localhost:5000" 
-  : "https://www.codekami.online";  
+  : "https://www.codekami.online/api";  
 
