b9c398b4bfd838bf4b4516f7534b95914f1bd446
Abhay-1718 <abhaysingh80766@gmail.com>
2025-02-19 11:36:17 +0530

    change constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-export const VITE_BACKEND_URL = 'http://15.206.179.46:5000';
-// export const VITE_BACKEND_URL=location.hostname === "localhost" ? "http://localhost:5000" : "/api";
+// export const VITE_BACKEND_URL = 'https://www.codekami.online';
+// export const VITE_BACKEND_URL = 'https://www.codekami.online/:5000';
+export const VITE_BACKEND_URL = location.hostname === "localhost" 
+  ? "http://localhost:5000" 
+  : "https://www.codekami.online/api";  
+
