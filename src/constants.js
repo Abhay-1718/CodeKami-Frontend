af6bc16775d35a76b37543d1f5306a12dbdec334
Abhay-1718 <abhaysingh80766@gmail.com>
2025-02-20 09:41:26 +0530

    update constant

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,6 @@
 // export const VITE_BACKEND_URL = 'https://www.codekami.online';
 // export const VITE_BACKEND_URL = 'https://www.codekami.online/:5000';
 // This needs to be modified in your constants.js
-export const VITE_BACKEND_URL =
-  location.hostname === "localhost"
+export const VITE_BACKEND_URL = location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://www.codekami.online";
+    : "http://15.206.179.46";
